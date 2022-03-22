a7acc17111a7211ecf7ec6102738f77f20352c35
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2022-03-22 20:16:38 +0200

    Start to create class Artillery Prior to this change, there was no object representation of the artillery
    This change creates a basic version of the class

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+class Artillery {
+private:
+    double m_degHeading;
+    double m_degElevation;
+    int m_maxRotSpeed;
+    int m_maxElevSpeed;
+public:
+    Artillery(double degHeading, double degElevation, int maxRotSpeed, int maxElevSpeed)
+        : m_degHeading(degHeading), m_degElevation(degElevation),
+          m_maxRotSpeed(maxRotSpeed), m_maxElevSpeed(maxElevSpeed) {};
+    Artillery(int maxRotSpeed, int maxElevSpeed)
+        : Artillery(0.0, 45.0, maxRotSpeed, maxElevSpeed) {};
+};
+
 int main() {
     std::cout << "Hello world!" << std::endl;
     return 0;
