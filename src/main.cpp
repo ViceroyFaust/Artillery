5da2c0ded232c2ad322f928b3328dd658520f7e3
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2022-03-25 15:43:28 +0200

    Introduce distance calculation to Artillery::shoot Prior to this change, Artillery:shoot did nothing
    This change uses a derived formula for the distance of a shot based on
    degree of elevation and initial velocity on level ground (shot and
    landing on the same height)
    
    Dx = 2 * Vnet^2 * sin(theta) * cos(theta) / g

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 
 class Artillery {
@@ -6,15 +7,26 @@ private:
     double m_degElevation;
     int m_maxRotSpeed;
     int m_maxElevSpeed;
+
+    double lvlGrndShotCalc(double shotVelocity) {
+        double elevDeg2Rad = m_degElevation * 3.1415 / 180;
+        // 2 * vnet^2 * sin(theta) * cos(theta) / g
+        return 2 * (shotVelocity*shotVelocity) * std::sin(elevDeg2Rad) * std::cos(elevDeg2Rad) / 9.81;
+    }
 public:
     Artillery(double degHeading, double degElevation, int maxRotSpeed, int maxElevSpeed)
         : m_degHeading(degHeading), m_degElevation(degElevation),
           m_maxRotSpeed(maxRotSpeed), m_maxElevSpeed(maxElevSpeed) {};
     Artillery(int maxRotSpeed, int maxElevSpeed)
         : Artillery(0.0, 45.0, maxRotSpeed, maxElevSpeed) {};
+
+    void shoot(double shotVelocity) {
+        std::cout << lvlGrndShotCalc(shotVelocity) << std::endl;
+    }
 };
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    Artillery art(0, 60, 10, 10);
+    art.shoot(30);
     return 0;
 }
