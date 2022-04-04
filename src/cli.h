9fc3c6ee3e5d48520238b846beb26c6b7344de67
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2022-04-04 15:34:59 +0300

    Move string functions from cli to owlFuncs Prior to this change, my string functions were in the cli file.
    This change moves them to owlFuncs for better organisation.

--- src/cli.h
+++ src/cli.h
@@ -6,12 +6,4 @@
 
 std::string getInput();
 
-std::vector<std::string> splitStr(const std::string& str, const std::string& delim);
-
-std::string leftTrim(const std::string& str);
-
-std::string rightTrim(const std::string& str);
-
-std::string trim(const std::string& str);
-
 #endif // CLI_H_INCLUDED
---
+++ src/owlFuncs.h
@@ -0,0 +1,12 @@
+#ifndef OWLFUNCS_H_INCLUDED
+#define OWLFUNCS_H_INCLUDED
+
+std::vector<std::string> splitStr(const std::string& str, const std::string& delim);
+
+std::string leftTrim(const std::string& str);
+
+std::string rightTrim(const std::string& str);
+
+std::string trim(const std::string& str);
+
+#endif // OWLFUNCS_H_INCLUDED
